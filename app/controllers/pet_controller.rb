1105ad98c3add86ec77cb9b8ba67ffa530f69463
msenior526 <msenior526@gmail.com>
2021-10-21 22:18:50 -0400

    Build create action in pet controller

--- app/controllers/pet_controller.rb
+++ app/controllers/pet_controller.rb
@@ -1,4 +1,14 @@
 class PetController < ApplicationController
+
+    def create
+        @pet = Pet.create(pet_params)
+
+        if @pet.valid? 
+            render json: { PetSerializer.new(@pet) }, status: :created
+        else
+            render json: { error: 'failed to create pet'}, status: :unprocessable_entity
+        end
+    end
     
     private
 

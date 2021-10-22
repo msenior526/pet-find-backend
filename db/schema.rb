12395e7d425e8d51c315bf57547e76499030af7b
msenior526 <msenior526@gmail.com>
2021-10-21 20:05:31 -0400

    Rename pet type column to species

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_235235) do
+ActiveRecord::Schema.define(version: 2021_10_22_000115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "species"
     t.integer "age"
     t.string "gender"
     t.bigint "user_id", null: false

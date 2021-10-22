f7d7b221c2eaf685f65b272bf9212f930f3f3d77
msenior526 <msenior526@gmail.com>
2021-10-21 20:50:44 -0400

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     has_many :pets
     has_many :saved_pets
     has_many :pets_saved, through: :saved_pets, source: :pet
+
+    validates :username, :email, presence: true, uniqueness: true
+    validates :name, :location, presence: true
 end

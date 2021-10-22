4ab003e51bbcdf91413cfc6e7f1833f5c73eb9b6
msenior526 <msenior526@gmail.com>
2021-10-21 21:18:57 -0400

    Add fake data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+    User.create(
+        name: Faker::Name.name,
+        username: Faker::Internet.password,
+        email: Faker::Internet.safe_email,
+        password: Faker::Internet.password,
+        location: Faker::Address.city + Faker::Address.state + Faker::Address.zip
+    )
+end
+
+20.times do 
+    Pet.create(
+        name: Faker::Creature::Dog.name,
+        species: Faker::Creature::Animal.name,
+        gender: Faker::Gender.binary_type,
+        age: Faker::Number.number(digits: 2),
+        user_id: Faker::Number.between(from: 1, to: User.all.count)
+    )
+end

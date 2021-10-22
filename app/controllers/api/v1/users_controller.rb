5783a1093715c72b78d78d0537d53e6aa2287589
msenior526 <msenior526@gmail.com>
2021-10-21 22:02:56 -0400

    Remove byebugs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,9 @@ class Api::V1::UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        byebug
         if @user.valid?
           render json: { user: UserSerializer.new(@user) }, status: :created
         else
-            byebug
           render json: { error: 'failed to create user' }, status: :unprocessable_entity
         end
       end    

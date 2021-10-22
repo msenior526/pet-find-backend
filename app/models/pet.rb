86b7888880e395e9e96af6032eff3efa166af0aa
msenior526 <msenior526@gmail.com>
2021-10-21 20:51:53 -0400

    Add validations to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,6 @@ class Pet < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
   has_many :saved_pets
   has_many :saved_by, through: :saved_pets, source: :user
+
+  validates :name, :species, :gender, presence: true
 end

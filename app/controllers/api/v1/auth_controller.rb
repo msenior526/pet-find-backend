1ec4abcd076c636f5e8daa6c19643bfa2a27d06b
msenior526 <msenior526@gmail.com>
2021-10-23 16:36:57 -0400

    Build create action for auth and send jwt with request

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only [:create]
 
+    def create
+        @user = User.find_by(email: user_params[:email])
+
+        if @user && @user.authenticate(user_params[:password]) 
+            token = encode_token({user: @user.id})
+            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+        else
+            render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
     private 
     def user_params
         params.require(:user).permit(:email, :password)

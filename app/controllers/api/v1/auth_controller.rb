fc6c90d505277f262c9b5a37072b456ce1d65a27
msenior526 <msenior526@gmail.com>
2021-10-23 16:31:11 -0400

    Use and create helpers for auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::AuthController < ApplicationController
+    skip_before_action :authorized, only [:create]
+
+    private 
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end

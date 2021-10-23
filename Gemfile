64a76f1baebe8f071ce2bc184dac95161b89d4a6
msenior526 <msenior526@gmail.com>
2021-10-23 12:18:51 -0400

    Add dot env to gemfile

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'rack-cors'
 gem 'jwt'
 gem 'active_model_serializers'
 gem 'faker'
+gem 'dotenv'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::API
     def encode_token(payload)
-        JWT.encode(payload, secret)
+        JWT.encode(payload, ENV['SECRET'])
+    end
+
+    def decode_token(token)
+        JWT.decode(token, ENV['SECRET'])
     end
 end

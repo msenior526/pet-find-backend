986fa89780c019ff049422337d0656d790ce3b23
msenior526 <msenior526@gmail.com>
2021-10-23 17:05:42 -0400

    Create method for current user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only [:create]
 
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+
     def create
         @user = User.find_by(email: user_params[:email])
 
